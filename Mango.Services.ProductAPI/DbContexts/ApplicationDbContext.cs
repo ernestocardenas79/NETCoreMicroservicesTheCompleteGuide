27e8465140e16437f37794ceb0f4ab0048cc0f37
UNOSQUARE\ernesto.cardenas <ernesto.cardenas@unosquare.com>
2023-03-27 11:27:04 -0700

    Initial commit based on 01BasicAPI

---
+++ Mango.Services.ProductAPI/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.ProductAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.DbContexts;
+
+public class ApplicationDbContext: DbContext
+{
+    public DbSet<Product> Products { get; set; }
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
