462e4bed2a0533f2e1a1327521b922ae8bf46901
UNOSQUARE\ernesto.cardenas <ernesto.cardenas@unosquare.com>
2024-03-01 18:39:30 -0700

    creating the web project

---
+++ Mango/Mango.Web/Service/ICouponService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service;
+
+public interface ICouponService
+{
+    Task<ResponseDto?> GetCouponAsync(string couponId);
+    Task<ResponseDto?> GetAllCouponsAsync();
+    Task<ResponseDto?> GetCouponByIdAsync(int id);
+    Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> UpdateCouponAsync(CouponDto couponDto);
+    Task<ResponseDto?> DeleteCouponAsync(int id);
+}
